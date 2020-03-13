ba137c66179e540a68d58528c70f4503a91f2198
jmr_sultan@hotmail.com <megacracks-1>
2020-03-13 20:31:10 +0100

    final huffman

--- 2.1Huffman/02.1_Huffman_PRACTICA.py
+++ 2.1Huffman/02.1_Huffman_PRACTICA.py
@@ -14,32 +14,60 @@ Dada una distribucion de probabilidad, hallar un código canónico de Huffman as
 '''
 
 
-iteracion = 0
-
 resource.setrlimit(resource.RLIMIT_STACK, (2**29,-1))
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**9)
 
+### CONVERSION CANONICA
+def suma_un_bit(c):
+    i = len(c)-1
+    added = False
+    while(i >= 0 and not added):
+        if (c[i] == '0'):
+            c = c[0:i] + '1' + c[i+1:]
+            added = True
+        else:
+            c = c[0:i] + '0' + c[i+1:]
+        i = i - 1
+    if (not added):
+        c = '1' + c
+    return c
+        
+
+def canonico(lista):
+    canonico = []
+    c = ''
+    for i in range(0,lista[0]):
+        c = c + '0'
+    canonico = canonico + [c]
+    for i in range(1,len(lista)):
+        c = suma_un_bit(c)
+        for i in range(0,lista[i]-lista[i-1]):
+            c = c + '0'
+        canonico = canonico + [c]
+    return canonico
+
+
+### GENERACION ARBOL
 def generacion_arbol(codigo):
-    if (len(codigo) == 1):
-        return codigo
-    else:
-        print("error ordenacion")
-        global iteracion 
-        iteracion = iteracion +1
-        print(iteracion)
-        if iteracion > 16882:
-            print(codigo)
-        codigo.sort()
-        print("error ordenacion 2")
-        suma_probabilidades = round(codigo[0][0] + codigo[1][0],10)
-        return generacion_arbol( [ [suma_probabilidades, codigo[0], codigo[1]] ] + codigo[2:len(codigo)] )
+    while (len(codigo) != 1):
+        minimo1 = min(codigo)
+        codigo.remove(minimo1)
+        minimo2 = min(codigo)
+        codigo.remove(minimo2)
+        suma_probabilidades = round(minimo1[0] + minimo2[0],10)
+        codigo = [ [suma_probabilidades, minimo1, minimo2] ] + codigo
+    return codigo
+    
 
+### RECORRER EL ARBOL
 def recorrer_arbol(codigos, c):
     if (len(codigos) == 1):
         return [ [codigos[0],c]]
     else:
         return recorrer_arbol(codigos[1], c+ '1') + recorrer_arbol(codigos[2], c + '0')
     
+def second(value):
+    return len(value[1])
 
 def Huffman(p):
     if len(p) == 1:
@@ -49,13 +77,17 @@ def Huffman(p):
         codigos = codigos + [[probabilidad]]
     codigos = generacion_arbol(codigos)
     codigo_huffman = recorrer_arbol(codigos[0],'')
-    return codigo_huffman
+    codigo_huffman.sort(key = second)
+    codigo_huffman_length = [ len(i[1]) for i in codigo_huffman]
+    codigo_huffman_canonico = canonico(codigo_huffman_length)
+    codigo_huffman_canonico = [[codigo_huffman[i][0] ,codigo_huffman_canonico[i]] for i in range(0, len(codigo_huffman))]
+    return codigo_huffman_canonico 
 
 p1=[0.05, 0.5, 0.1, 0.25, 0.05, 0.05]
 p2=[0.102, 0.106, 0.053, 0.114, 0.0081, 0.106, 0.0088, 0.030, 0.056, 0.055, 0.032, 0.094, 0.0075, 0.078, 0.1496]
 
-#print(Huffman(p1))
-#print(Huffman(p2))
+print(Huffman(p1))
+print(Huffman(p2))
 '''
 p=[0.05, 0.5, 0.1, 0.25, 0.05, 0.05]
 Huffman(p)=['11110', '0', '110', '10', '11111', '1110'] (¡NO ES ÚNICO!)
@@ -181,7 +213,7 @@ def obtener_m2c(huffman,tablaFrecuencias):
 def EncodeHuffman(mensaje_a_codificar,numero_de_simbolos=1):
     t0 = time.time()
     longitud_mensaje = len(mensaje_a_codificar)
-    residuo = longitud_mensaje-((int(longitud_mensaje/numero_de_simbolos)+1)*numero_de_simbolos);
+    residuo = ((int(longitud_mensaje/numero_de_simbolos)+1)*numero_de_simbolos) - longitud_mensaje;
     if longitud_mensaje% numero_de_simbolos != 0:
         for i in range(0,residuo):
             mensaje_a_codificar = mensaje_a_codificar + '.'
@@ -203,9 +235,9 @@ def DecodeHuffman(mensaje_codificado,m2c,longitud_mensaje):
 
 mensaje='La heroica ciudad dormía la siesta. El viento Sur, caliente y perezoso, empujaba las nubes blanquecinas que se rasgaban al correr hacia el Norte. En las calles no había más ruido que el rumor estridente de los remolinos de polvo, trapos, pajas y papeles que iban de arroyo en arroyo, de acera en acera, de esquina en esquina revolando y persiguiéndose, como mariposas que se buscan y huyen y que el aire envuelve en sus pliegues invisibles. Cual turbas de pilluelos, aquellas migajas de la basura, aquellas sobras de todo se juntaban en un montón, parábanse como dormidas un momento y brincaban de nuevo sobresaltadas, dispersándose, trepando unas por las paredes hasta los cristales temblorosos de los faroles, otras hasta los carteles de papel mal pegado a las esquinas, y había pluma que llegaba a un tercer piso, y arenilla que se incrustaba para días, o para años, en la vidriera de un escaparate, agarrada a un plomo. Vetusta, la muy noble y leal ciudad, corte en lejano siglo, hacía la digestión del cocido y de la olla podrida, y descansaba oyendo entre sueños el monótono y familiar zumbido de la campana de coro, que retumbaba allá en lo alto de la esbeltatorre en la Santa Basílica. La torre de la catedral, poema romántico de piedra,delicado himno, de dulces líneas de belleza muda y perenne, era obra del siglo diez y seis, aunque antes comenzada, de estilo gótico, pero, cabe decir, moderado por un instinto de prudencia y armonía que modificaba las vulgares exageraciones de esta arquitectura. La vista no se fatigaba contemplando horas y horas aquel índice depiedra que señalaba al cielo; no era una de esas torres cuya aguja se quiebra desutil, más flacas que esbeltas, amaneradas, como señoritas cursis que aprietandemasiado el corsé; era maciza sin perder nada de su espiritual grandeza, y hasta sussegundos corredores, elegante balaustrada, subía como fuerte castillo, lanzándosedesde allí en pirámide de ángulo gracioso, inimitable en sus medidas y proporciones.Como haz de músculos y nervios la piedra enroscándose en la piedra trepaba a la altura, haciendo equilibrios de acróbata en el aire; y como prodigio de juegosmalabares, en una punta de caliza se mantenía, cual imantada, una bola grande debronce dorado, y encima otra más pequenya, y sobre ésta una cruz de hierro que acababa en pararrayos.'
 
-# mensaje_codificado, m2c, longitud_mensaje = EncodeHuffman(mensaje,numero_de_simbolos=1)
-# print(DecodeHuffman(mensaje_codificado,m2c,longitud_mensaje) )
 
+#mensaje_codificado, m2c, longitud_mensaje = EncodeHuffman(mensaje,numero_de_simbolos=1)
+#print(DecodeHuffman(mensaje_codificado,m2c,longitud_mensaje) )
 
 """
 Ejemplo
@@ -228,6 +260,8 @@ longitud_mensaje=9834
 
 
 
+
+
 '''
 Si no tenemos en cuenta la memoria necesaria para almacenar el diccionario, 
 haced una estimación de la ratio de compresión para el fichero "la_regenta"
@@ -238,19 +272,30 @@ haced una estimación de la ratio de compresión.
 Repetid las estimaciones con numero_de_simbolos=2,3,...
 '''
 
-with open ("la_regenta.txt", "r") as myfile:
-    mensaje=myfile.read()
-    EncodeHuffman(mensaje,4)
-"""
-n = 5
+
+
+n = 10
 with open ("la_regenta.txt", "r") as myfile:
     mensaje=myfile.read()
     tamaño_original = len(mensaje)
     for numero_de_simbolos in range(1,n+1):
+        print("numero de simbolos")
         print(numero_de_simbolos)
-        _ , _, tamaño_comprimido = EncodeHuffman(mensaje,numero_de_simbolos)
+        mensaje_codificado, m2c, longitud_mensaje = EncodeHuffman(mensaje,numero_de_simbolos)
+        #El tamaño original se guarda en chars cada chars son 8 bits en contraposición
+        #el mensaje comprimido se guarda en bits por lo que no se multiplica por 8
+        tamaño_comprimido = len(mensaje_codificado)
+        print("ratio de compresion sin diccionario")
         print(tamaño_original*8/tamaño_comprimido)
-"""
+        
+        #El tamaño original se guarda en chars cada chars son 8 bits en contraposición
+        #el mensaje comprimido se guarda en bits por lo que no se multiplica por 8
+        #En el diccionario se guarda el simbolo mas el tamaño de codificacion del simbolo ya que el mensaje codificado es canonico
+        #Por lo que el diccionario es numero de simbolos por numero de codificaciones mas numero de simbolos por tamaño de la codificacion (32 bits = int)
+        diccionario = numero_de_simbolos*len(m2c)+ len(m2c)*32
+        print("ratio de compresion con diccionario")
+        print(tamaño_original*8/(tamaño_comprimido+diccionario))
+
     
     
 
