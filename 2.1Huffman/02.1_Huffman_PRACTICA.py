977f3cc64ef768889053c8d763cc41bdad3229ab
jmr_sultan@hotmail.com <megacracks-1>
2020-03-13 12:50:31 +0100

    error recursive

--- 2.1Huffman/02.1_Huffman_PRACTICA.py
+++ 2.1Huffman/02.1_Huffman_PRACTICA.py
@@ -14,7 +14,7 @@ Dada una distribucion de probabilidad, hallar un código canónico de Huffman as
 '''
 
 
-
+iteracion = 0
 
 resource.setrlimit(resource.RLIMIT_STACK, (2**29,-1))
 sys.setrecursionlimit(10**6)
@@ -23,7 +23,14 @@ def generacion_arbol(codigo):
     if (len(codigo) == 1):
         return codigo
     else:
+        print("error ordenacion")
+        global iteracion 
+        iteracion = iteracion +1
+        print(iteracion)
+        if iteracion > 16882:
+            print(codigo)
         codigo.sort()
+        print("error ordenacion 2")
         suma_probabilidades = round(codigo[0][0] + codigo[1][0],10)
         return generacion_arbol( [ [suma_probabilidades, codigo[0], codigo[1]] ] + codigo[2:len(codigo)] )
 
@@ -174,11 +181,10 @@ def obtener_m2c(huffman,tablaFrecuencias):
 def EncodeHuffman(mensaje_a_codificar,numero_de_simbolos=1):
     t0 = time.time()
     longitud_mensaje = len(mensaje_a_codificar)
-    residuo = longitud_mensaje% numero_de_simbolos
-    if residuo != 0:
+    residuo = longitud_mensaje-((int(longitud_mensaje/numero_de_simbolos)+1)*numero_de_simbolos);
+    if longitud_mensaje% numero_de_simbolos != 0:
         for i in range(0,residuo):
             mensaje_a_codificar = mensaje_a_codificar + '.'
-    
     tablafrecuencias = tablaFrecuencias(mensaje_a_codificar, numero_de_simbolos)
     p = [probabilidad for [_, probabilidad] in tablafrecuencias]
     huffman  = Huffman(p)
@@ -189,7 +195,6 @@ def EncodeHuffman(mensaje_a_codificar,numero_de_simbolos=1):
     
     
 def DecodeHuffman(mensaje_codificado,m2c,longitud_mensaje):
-    print("decode")
     c2m = dict([(c,m) for (m,c) in m2c])
     mensaje_decodificado = Encode(mensaje_codificado,c2m)
     mensaje_decodificado = mensaje_decodificado[0:longitud_mensaje]
@@ -198,8 +203,8 @@ def DecodeHuffman(mensaje_codificado,m2c,longitud_mensaje):
 
 mensaje='La heroica ciudad dormía la siesta. El viento Sur, caliente y perezoso, empujaba las nubes blanquecinas que se rasgaban al correr hacia el Norte. En las calles no había más ruido que el rumor estridente de los remolinos de polvo, trapos, pajas y papeles que iban de arroyo en arroyo, de acera en acera, de esquina en esquina revolando y persiguiéndose, como mariposas que se buscan y huyen y que el aire envuelve en sus pliegues invisibles. Cual turbas de pilluelos, aquellas migajas de la basura, aquellas sobras de todo se juntaban en un montón, parábanse como dormidas un momento y brincaban de nuevo sobresaltadas, dispersándose, trepando unas por las paredes hasta los cristales temblorosos de los faroles, otras hasta los carteles de papel mal pegado a las esquinas, y había pluma que llegaba a un tercer piso, y arenilla que se incrustaba para días, o para años, en la vidriera de un escaparate, agarrada a un plomo. Vetusta, la muy noble y leal ciudad, corte en lejano siglo, hacía la digestión del cocido y de la olla podrida, y descansaba oyendo entre sueños el monótono y familiar zumbido de la campana de coro, que retumbaba allá en lo alto de la esbeltatorre en la Santa Basílica. La torre de la catedral, poema romántico de piedra,delicado himno, de dulces líneas de belleza muda y perenne, era obra del siglo diez y seis, aunque antes comenzada, de estilo gótico, pero, cabe decir, moderado por un instinto de prudencia y armonía que modificaba las vulgares exageraciones de esta arquitectura. La vista no se fatigaba contemplando horas y horas aquel índice depiedra que señalaba al cielo; no era una de esas torres cuya aguja se quiebra desutil, más flacas que esbeltas, amaneradas, como señoritas cursis que aprietandemasiado el corsé; era maciza sin perder nada de su espiritual grandeza, y hasta sussegundos corredores, elegante balaustrada, subía como fuerte castillo, lanzándosedesde allí en pirámide de ángulo gracioso, inimitable en sus medidas y proporciones.Como haz de músculos y nervios la piedra enroscándose en la piedra trepaba a la altura, haciendo equilibrios de acróbata en el aire; y como prodigio de juegosmalabares, en una punta de caliza se mantenía, cual imantada, una bola grande debronce dorado, y encima otra más pequenya, y sobre ésta una cruz de hierro que acababa en pararrayos.'
 
-mensaje_codificado, m2c, longitud_mensaje = EncodeHuffman(mensaje,numero_de_simbolos=1)
-print(DecodeHuffman(mensaje_codificado,m2c,longitud_mensaje) )
+# mensaje_codificado, m2c, longitud_mensaje = EncodeHuffman(mensaje,numero_de_simbolos=1)
+# print(DecodeHuffman(mensaje_codificado,m2c,longitud_mensaje) )
 
 
 """
@@ -233,7 +238,10 @@ haced una estimación de la ratio de compresión.
 Repetid las estimaciones con numero_de_simbolos=2,3,...
 '''
 
-
+with open ("la_regenta.txt", "r") as myfile:
+    mensaje=myfile.read()
+    EncodeHuffman(mensaje,4)
+"""
 n = 5
 with open ("la_regenta.txt", "r") as myfile:
     mensaje=myfile.read()
@@ -242,7 +250,7 @@ with open ("la_regenta.txt", "r") as myfile:
         print(numero_de_simbolos)
         _ , _, tamaño_comprimido = EncodeHuffman(mensaje,numero_de_simbolos)
         print(tamaño_original*8/tamaño_comprimido)
-    
+"""
     
     
 
