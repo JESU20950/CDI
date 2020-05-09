2649110a1e5c9e6fcbe1a6749928d0a26780ee13
jesus <jmr_sultan@hotmail.com>
2020-05-09 23:20:28 +0200

    jpeg

--- 3.Compresion_con_perdidas/3.2Compresion_de_imagenes_JPEG/07_jpeg_PRACTICA.py
+++ 3.Compresion_con_perdidas/3.2Compresion_de_imagenes_JPEG/07_jpeg_PRACTICA.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-"""
+'''
 
-"""
+'''
 
 import numpy as np
 import scipy
@@ -17,9 +17,9 @@ import matplotlib.pyplot as plt
 
 
         
-"""
+'''
 Matrices de cuantización, estándares y otras
-"""
+'''
 
     
 Q_Luminance=np.array([
@@ -49,7 +49,7 @@ def Q_matrix(r=1):
             m[i,j]=(1+i+j)*r
     return m
 
-"""
+'''
 Implementar la DCT (Discrete Cosine Transform) 
 y su inversa para bloques NxN (podéis utilizar si quereis el paquete scipy.fftpack)
 
@@ -58,19 +58,86 @@ idct_bloque(p,N)
 
 p bloque NxN
 
-"""
+'''
 def dct_bloque(p):
+    (n,_) = p.shape
+    w = np.zeros((n,n))
+    for i in range(0, n):
+        for j in range(0, n):
+            ci = 1
+            cj = 1
+            if (i == 0):
+                ci = 1/math.sqrt(2)
+            if (j == 0):
+                cj = 1/math.sqrt(2)
+            suma = 0
+            for x in range(0,n):
+                for y in range(0,n):
+                    suma = suma + (p[x,y]* math.cos( (2*x+1)*i*math.pi/(2*n))* math.cos( (2*y+1)*j*math.pi/(2*n)))
+            
+            w[i,j] = (1/math.sqrt(2*n))*ci*cj*suma
+    return w
 
-def idct_bloque(p):
+def idct_bloque(w):
+    (n,_) = w.shape
+    p = np.zeros((n,n))
+    for x in range(0,n):
+        for y in range(0,n):
+            suma = 0
+            for i in range(0,n):
+                for j in range(0,n):
+                    ci = 1
+                    cj = 1
+                    if (i == 0):
+                        ci = 1/math.sqrt(2)
+                    if (j == 0):
+                        cj = 1/math.sqrt(2)
+                    suma = suma + (ci*cj*w[i,j]* math.cos( (2*x+1)*i*math.pi/(2*n))* math.cos( (2*y+1)*j*math.pi/(2*n)))
+            p[x,y] = (1/math.sqrt(2*n))*suma
+    return p
 
+def dctmtx(N):
+    dct_matrix = np.zeros((N,N));
+    for i in range(0,N):
+        for j in range(0,N):
+            if (i == 0):
+                dct_matrix[i,j] = 1/math.sqrt(N)
+            else:
+                dct_matrix[i,j] = math.sqrt(2/N)*math.cos((2*j+1)*i*math.pi/(2*N))
+    return dct_matrix
 
-"""
+def dct_bloque_v2(p):
+    (N,_) = p.shape
+    c = dctmtx(N)
+    w = c*p*np.transpose(c)
+    return w
+
+def idct_bloque_v2(w):
+    (N,_) = p.shape
+    c = dctmtx(N)
+    p = np.transpose(c)*w*c
+    return p
+
+'''
 Reproducir los bloques base de la transformación para los casos N=4,8
 Ver imágenes adjuntas.
-"""
+'''
 
+            
+    
+    
+def basis_function_blocks(N):
+    dct_matrix = dctmtx(N)
+    zeros = np.zeros((N,N,N,N))
+    for i in range(0,N)
+        for 
+    
+    
+    
+#basis_function_images(2**3)
+    
 
-"""
+'''
 Implementar la función jpeg_gris(imagen_gray) que: 
 1. dibuje el resultado de aplicar la DCT y la cuantización 
 (y sus inversas) a la imagen de grises 'imagen_gray' 
@@ -83,12 +150,12 @@ según los coeficientes nulos de la transformación:
 Sigma=np.sqrt(sum(sum((imagen_gray-imagen_jpeg)**2)))/np.sqrt(sum(sum((imagen_gray)**2)))
 
 
-"""
+'''
 
 def jpeg_gris(imagen_gray):
+    return
 
-
-"""
+''''
 Implementar la función jpeg_color(imagen_color) que: 
 1. dibuje el resultado de aplicar la DCT y la cuantización 
 (y sus inversas) a la imagen RGB 'imagen_color' 
@@ -100,16 +167,17 @@ según los coeficientes nulos de la transformación:
 3. haga una estimación del error para cada una de las componentes RGB
 Sigma=np.sqrt(sum(sum((imagen_color-imagen_jpeg)**2)))/np.sqrt(sum(sum((imagen_color)**2)))
 
-"""
+'''
 
 def jpeg_color(imagen_color):
+    return
 
-"""
+'''
 #--------------------------------------------------------------------------
 Imagen de GRISES
 
 #--------------------------------------------------------------------------
-"""
+'''
 
 
 ### .astype es para que lo lea como enteros de 32 bits, si no se
@@ -124,11 +192,11 @@ end= time.clock()
 print("tiempo",(end-start))
 
 
-"""
+'''
 #--------------------------------------------------------------------------
 Imagen COLOR
 #--------------------------------------------------------------------------
-"""
+'''
 
 mandril_color=scipy.misc.imread('./mandril_color.png').astype(np.int32)
 
